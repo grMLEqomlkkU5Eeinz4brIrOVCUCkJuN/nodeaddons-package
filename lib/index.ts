7c3635c9baf49e14610ea612e495d7ca31c86e2c
grml <grml@grml.com>
2025-12-14 02:55:15 +0800

    ci: updated pipeline to build and release to npm

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,5 @@
 // It is important to do the following steps to import the native addon in an ESM context, it needs to be understood that since the native addon is a compiled C++ project
-// typescript cannot import and read the types and other relevant information from the addon. 
+// typescript cannot import and read the types and other relevant information from the addon.
 // To circumvent this, we use the createRequire function to create a require function for this ESM module and use that to import the addon.
 
 // I highly recommend that you read this repository to understand more: https://github.com/xan105/node-addons/blob/main/lib/napi.js
@@ -8,12 +8,17 @@
 
 // Import createRequire to use require() in ESM context
 import { createRequire } from "module";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
 
 // Create a require function for this ESM module
 const require = createRequire(import.meta.url);
 
-// Import the native addon using the created require function
-const addon = require("../build/Release/addon") as { hello(): string };
+// Get the directory of this module
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// Import the native addon using node-gyp-build which will load prebuilt binaries or fall back to build directory
+const addon = require("node-gyp-build")(join(__dirname, "..")) as { hello(): string };
 
 
 
